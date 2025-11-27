dda8b50fde309415cdf81d495bd03d6385150872
Guillaume <guillaume.thouvenin@vates.tech>
2025-11-27 15:11:44 +0100

    Add a run step the start server, wait and start client

--- build.zig
+++ build.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+
+    // ---- Build Zig client
     const exe = b.addExecutable(.{
         .name = "zclient",
         .root_module = b.createModule(.{
@@ -12,6 +14,7 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
+    // ----- Build Ocaml server via dune
     const dune_build = b.step("dune", "Build Ocaml project using dune");
     const cmd = b.addSystemCommand(&.{ "dune", "build" });
     cmd.cwd = b.path("frameforge");
@@ -19,4 +22,33 @@ pub fn build(b: *std.Build) void {
     dune_build.dependOn(&cmd.step);
     // Add it in default target
     b.default_step.dependOn(dune_build);
+
+    // ----- Add a run step
+    // we want to:
+    //   - start the server
+    //   - wait that socket /tmp/frameforge.socket is ready
+    //   - start the client
+    const run_step = b.step("run", "Run frameforge and zclient");
+
+    // start frameforge in the background and wait for socket to be ready
+    const server = b.addSystemCommand(&.{
+        "sh",
+        "-c",
+        \\ ./frameforge/_build/default/bin/main.exe &
+        \\ echo "Waiting for server to be ready"
+        \\ for i in $(seq 1 50); do
+        \\   [ -S /tmp/frameforge.socket ] && exit 0
+        \\   sleep 0.1
+        \\ done
+        \\ echo "Failed to get /tmp/frameforge.socket ready; timed out."
+        \\ exit 1
+        ,
+    });
+
+    // Run the client when server is ready
+    const client = b.addRunArtifact(exe);
+    client.step.dependOn(&server.step);
+
+    // Finally we have all the parts
+    run_step.dependOn(&client.step);
 }
