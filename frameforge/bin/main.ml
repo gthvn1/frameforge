3a5dc5f4dc0ca174346fdb8bd97667ef0f9e5c4d
Guillaume <gthvn1@gmail.com>
2025-11-29 21:29:19 +0100

    Allow passing --test to frameforge

--- frameforge/bin/main.ml
+++ frameforge/bin/main.ml
@@ -1,2 +1,4 @@
 (* TODO: use args to set test or not *)
-let () = Frameforge.Server.ping_pong ~is_test:true "/tmp/frameforge.socket"
+let () =
+  let is_test = Array.mem  "--test" Sys.argv in
+  Frameforge.Server.ping_pong ~is_test "/tmp/frameforge.socket"
