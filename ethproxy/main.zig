e5a208c32045771d757457b5bdb383186378f992
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-07 17:43:54 +0100

    We can now send multiple request to server

--- ethproxy/main.zig
+++ ethproxy/main.zig
@@ -46,13 +46,38 @@ pub fn main() !void {
     }
 
     // At this point the network should be up and running.
-
-    simple_client() catch |err| {
+    simpleClient() catch |err| {
         std.debug.print("Failed to run client: {}\n", .{err});
     };
 }
 
-fn simple_client() !void {
+// We are using a global variable to quit loop so handler
+// of sigint can set it. Not sure if it is the correct solution.
+// As it is global let's use an atomic one...
+var quit_loop = std.atomic.Value(bool).init(false);
+
+fn handleSigint(sig: c_int) callconv(.c) void {
+    _ = sig;
+    quit_loop.store(true, .release);
+}
+
+fn simpleClient() !void {
+    // At this point the network should be up and running.
+    // We will go to the infinite loop but before we need to
+    // setup a signal handler for Sigint to be able to quit properly
+    // and cleanup network.
+    //   - man signal
+    //   - man 7 signal
+    const signalInterrupt = std.posix.Sigaction{
+        .handler = .{ .handler = handleSigint },
+        .mask = std.posix.sigemptyset(),
+        .flags = 0,
+    };
+
+    // Change the signal action SIGINT that is "interrupt from keyboard"
+    std.posix.sigaction(std.posix.SIG.INT, &signalInterrupt, null);
+    std.debug.print("You can quit using Ctrl-C\n", .{});
+
     // man 7 unix
     // -> We use AF_UNIX to communicate locally between processes
     const sock = try posix.socket(posix.AF.UNIX, posix.SOCK.STREAM, 0);
@@ -69,35 +94,42 @@ fn simple_client() !void {
     try posix.connect(sock, @ptrCast(&laddr), @sizeOf(posix.sockaddr.un));
     defer posix.close(sock);
 
-    // Ask to the user what to send
-    std.debug.print("> ", .{});
     var stdin_buffer: [64]u8 = undefined;
     var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
     const stdin = &stdin_reader.interface;
-    const msg = try stdin.takeDelimiterExclusive('\n');
 
-    // We are using a simple protocol where we send the size of the data
-    // and then the data.
-    // We use an array of 4 bytes to be sure that it will be send using
-    // the correct format.
-    var header: [4]u8 = undefined; // will contain the size of the msg
-    std.mem.writeInt(u32, &header, @intCast(msg.len), .little);
-    _ = try posix.send(sock, &header, 0);
-    _ = try posix.send(sock, msg, 0);
+    loop: while (!quit_loop.load(.acquire)) {
+        // Ask to the user what to send
+        std.debug.print("> ", .{});
+        const msg = try stdin.takeDelimiterExclusive('\n');
+        // consume the '\n'
+        _ = try stdin.take(1);
 
-    // And wait for the response...
-    var buf: [64]u8 = undefined;
-    const n = try posix.recv(sock, &buf, 0);
+        std.debug.print("READ <{s}>\n", .{msg});
 
-    if (n < 4) {
-        std.debug.print("We should at least received 4 bytes, received {d}\n", .{n});
-        return;
+        // We are using a simple protocol where we send the size of the data
+        // and then the data.
+        // We use an array of 4 bytes to be sure that it will be send using
+        // the correct format.
+        var header: [4]u8 = undefined; // will contain the size of the msg
+        std.mem.writeInt(u32, &header, @intCast(msg.len), .little);
+        _ = try posix.send(sock, &header, 0);
+        _ = try posix.send(sock, msg, 0);
+
+        // And wait for the response...
+        var buf: [64]u8 = undefined;
+        const n = try posix.recv(sock, &buf, 0);
+
+        if (n < 4) {
+            std.debug.print("We should at least received 4 bytes, received {d}\n", .{n});
+            continue :loop;
+        }
+
+        // The first four bytes are the size and then the data
+        const data_len: u32 = std.mem.readInt(u32, buf[0..4], .little);
+        std.debug.print("ETHPROXY: Data size: {d} \n", .{data_len});
+        std.debug.print("ETHPROXY: Payload  : {s}\n", .{buf[4 .. 4 + data_len]});
     }
 
-    // The first four bytes are the size and then the data
-    const data_len: u32 =
-        @as(u32, buf[0]) | @as(u32, buf[1]) << 8 | @as(u32, buf[2]) << 16 | @as(u32, buf[3]) << 24;
-
-    std.debug.print("ETHPROXY: Data size: {d} \n", .{data_len});
-    std.debug.print("ETHPROXY: Payload  : {s}\n", .{buf[4 .. 4 + data_len]});
+    std.debug.print("Break out of the loop cleanly\n", .{});
 }
