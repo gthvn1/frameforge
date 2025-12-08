dd99628bda3f4c16e9faa4e1aaa1a129b7a176e2
Guillaume <gthvn1@gmail.com>
2025-12-08 22:25:45 +0100

    Debug the problem where data size on ethproxy is too big
    see Readme for details about debugging

--- frameforge/bin/main.ml
+++ frameforge/bin/main.ml
@@ -1,12 +1,3 @@
 let () =
   let open Frameforge in
-  let run_once = Array.mem "--once" Sys.argv in
-  (* Currently only pong is available but later we will have another one *)
-  let handler =
-    if Array.mem "--pong" Sys.argv then
-      Pong_handler.handle
-    else
-      Ethernet_handler.handle
-  in
-
-  Server.run ~run_once "/tmp/frameforge.socket" handler
+  Server.run "/tmp/frameforge.socket" Ethernet_handler.handle
