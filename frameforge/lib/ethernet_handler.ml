4674b3c41407419f6b4bc19cf29b154c957ef861
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-10 11:43:36 +0100

    add comments

--- frameforge/lib/ethernet_handler.ml
+++ frameforge/lib/ethernet_handler.ml
@@ -3,8 +3,11 @@ let hex_of_bytes (b : bytes) : string list =
   |> List.rev
 
 let handle (payload : bytes) : bytes =
+  (* Just display payload as hexadecimal. Next step is to parse the
+     ethertype... and decode the payload... *)
   let sl = hex_of_bytes payload in
-  (* Insert a new line each 8 elements *)
+  (* Insert a new line each 8 bytes *)
   List.mapi (fun i s -> if i > 0 && i mod 8 = 0 then "\n" ^ s else s) sl
   |> String.concat " " |> print_endline;
+  (* just return a dummy message*)
   Bytes.of_string "TODO: parse ethernet frame\n"
